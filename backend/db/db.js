de3fa912ab22c5d48f440bb164b70ecaf05e88a5
Hari <amarhari26122002@gmail.com>
2026-07-04 12:56:45 +0530

    database is connected

---
+++ backend/db/db.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
+
+const dbConnection = async() =>{
+     const url=process.env.MONGODB_URI
+     
+
+    try{
+       
+
+       const dbConnect = await mongoose.connect(url);
+
+ 
+       console.log('the database is connected ' )
+
+
+
+    }
+
+    catch(error){
+    
+        console.log('database connection is failed' );
+
+        console.error(error.message);
+
+        process.exit(1);
+
+        
+    }
+
+
+
+}
+
+module.exports = dbConnection;
