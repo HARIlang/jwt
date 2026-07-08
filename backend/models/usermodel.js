c7bf5e282d3a9b3067239dc0a49b4fbbde518c71
Hari <amarhari26122002@gmail.com>
2026-07-08 13:20:48 +0530

    user models are added

---
+++ backend/models/usermodel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { timeStamp } = require("node:console");
+
+const userSchema = mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: email,
+    unique: true,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+},
+{
+    timeStamp:true
+});
+
+const userModel = mongoose.model('user',userSchema);
+
+module.exports = userModel
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
 const db = require('./db/db.js');
 db();
+
+const port = process.env.PORT
+
+
+
+app.listen(port,()=>console.log('the server is running'))
