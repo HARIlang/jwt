256518f4450b21f6e3cbc813c41a1939cd06c7f1
Hari <amarhari26122002@gmail.com>
2026-07-09 12:29:47 +0530

    router is added for signup

---
+++ backend/routes/router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const signup = require('../controller/user/signup.js')
+
+router.get('/signup');
+
+module.exports = router
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const router = require('./routes/router.js');
+
+
 
 app.use(express.json());
 
 const db = require('./db/db.js');
 db();
+
+app.use('/api',router);
 
 const port = process.env.PORT
 
